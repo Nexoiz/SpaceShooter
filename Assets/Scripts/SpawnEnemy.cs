2662e7587e39282f386a41880fb7f34a78423a83
Nexoiz <mail2nex@gmail.com>
2014-06-09 12:46:39 +0300

    Enemy spawning

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class SpawnEnemy : MonoBehaviour {
 
+	public GameObject enemy;
+	Transform transform;
+	void Start () {
+		transform = GetComponent<Transform>();
+		InvokeRepeating("CreateEnemy",0.01f,2.0f);
+	}
 
-
+	
+	void CreateEnemy(){
+		Vector3 enemyVec = new Vector3(transform.position.x,Random.Range (-5f,5f),0);
+		Instantiate(enemy,enemyVec,transform.rotation);
+	}
 }
