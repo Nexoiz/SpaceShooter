891fd1f46db80ef95ac06c1ca51e08853f7cc8c2
unknown <kasperi.koskinen@PELILAPTOP08.opisto.hkro>
2014-06-11 12:05:33 +0300

    added sounds

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class Projectile : MonoBehaviour {
 
 	public int damage;
+	public AudioClip clip;
 
 	void OnCollisionEnter2D (Collision2D col) {
 		if (col.gameObject.CompareTag ("Enemy")) 
 		{
 			Destroy(col.gameObject);
 			Destroy (gameObject);
+			AudioSource.PlayClipAtPoint(clip, transform.position);
 		}
 	}
 
