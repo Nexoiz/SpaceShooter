54d0774df1244892f161008a6654a7e732d6d879
Edmash <emil.dewald@outlook.com>
2014-06-17 11:56:11 +0300

    soundtrack

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Music : MonoBehaviour {
+	static Music instance = null;
+	void Start () {
+		if (instance == null) {
+			instance = this;
+
+		}
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+	
+}
